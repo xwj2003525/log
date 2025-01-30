32b92aebd8429ecfb61209b5f9e306264bdff2a7
xwj2003525 <xwj2003525@gmail.com>
2025-01-30 20:41:52 +0800

    Create log.h

---
+++ log.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "common.h"
+#include <cstdlib>
+#include <string>
+
+enum class level { TRACE, DEBUG, INFO, WARN, ERROR, FATAL };
+
+void initlevel(level);
+
+struct synclog {
+
+  synclog(const char *tid, level l, const char *filename, int line,
+          const char *func);
+  ~synclog();
+  struct stream &stream();
+
+private:
+  level l_;
+  struct stream buf_;
+};
+
+#define LOG(severity)                                                          \
+  if (getlevel() <= level::severity)                                           \
+  synclog(get_tid(), level::severity, __FILE__, __LINE__, __func__).stream()
