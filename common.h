42331b4fccf257a00ff85198e56ffab3a9eca324
xwj2003525 <xwj2003525@gmail.com>
2025-01-30 20:42:44 +0800

    Create common.h

---
+++ common.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "buf.hpp"
+#include "timestamp.hpp"
+
+using timestamp = timestamp;
+using Smallbuf = buf<4096>;
+
+struct stream {
+
+  NO_COPY(stream);
+  stream() = default;
+  template <class T> stream &operator<<(T);
+
+  stream &operator<<(bool);
+  stream &operator<<(char);
+  stream &operator<<(const char *);
+  stream &operator<<(const std::string &);
+  stream &operator<<(const Smallbuf &);
+
+  void add(const char *, size_t len);
+  const Smallbuf &buf() const;
+  void reset();
+
+private:
+  Smallbuf _buf;
+};
+
+template <class T> inline stream &stream::operator<<(T t) {
+  *this << std::to_string(t);
+  return *this;
+}
+
+void add_meta(const char *time, const char *tid, const char *level,
+              const char *filename, const char *func, int line, stream &);
+
+const char *get_tid();
